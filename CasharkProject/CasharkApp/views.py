8f83e130765e3bd620b909eb881f84bfd749a4ca
chingpoi <87354186+chingpoi@users.noreply.github.com>
2021-11-10 19:08:38 +0800

    papasd

--- CasharkProject/CasharkApp/views.py
+++ CasharkProject/CasharkApp/views.py
@@ -2,6 +2,34 @@ from django.shortcuts import redirect, render
 from django.views.generic import View
 
 # Create your views here.
+class AboutView(View):
+	def get(self,request):
+		return render(request,'about.html')
+
+class BlogSingleView(View):
+	def get(self,request):
+		return render(request,'blog.html')
+
+class BlogView(View):
+	def get(self,request):
+		return render(request,'contact.html')
+
+class ContactView(View):
+	def get(self,request):
+		return render(request,'index.html')
+
 class IndexView(View):
+	def get(self,request):
+		return render(request,'login.html')
+
+class LoginView(View):
+	def get(self,request):
+		return render(request,'portfolio.html')
+
+class PortfolioView(View):
+	def get(self,request):
+		return render(request,'services.html')
+
+class ServicesView(View):
 	def get(self,request):
 		return render(request,'index.html')
