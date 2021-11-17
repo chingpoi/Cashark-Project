852f2bfa67d4507a4025136813ea9913cc25664e
chingpoi <87354186+chingpoi@users.noreply.github.com>
2021-11-17 20:58:55 +0800

    Ver 1.3.5 - Full Session
    Full Session has been achieved, should reflect to profile

--- CasharkProject/CasharkApp/views.py
+++ CasharkProject/CasharkApp/views.py
@@ -71,7 +71,21 @@ class Functions(View):
 					form = User(First_Name = firstName, Last_Name = lastName, Email = email,  Password = password, Birthdate = birthdate, Mobile_Number = mobileNumber, Address_ID = AddressID, Balance = 0, Credit_Score = 0)
 					form.save()
 
-					request.session['First_Name'] = firstName
+					user = User.objects.get(Email = email)
+
+					request.session['User_ID'] = user.User_ID
+					request.session['First_Name'] = user.First_Name
+					request.session['Last_Name'] = user.Last_Name
+					request.session['Email'] = user.Email
+					request.session['Birthdate'] = user.Birthdate
+					request.session['Mobile_Number'] = user.Mobile_Number
+					request.session['Balance'] = user.Balance
+					request.session['Credit_Score'] = user.Credit_Score
+
+					request.session['Address_Province'] = user.Address_ID.Address_Province
+					request.session['Address_City'] = user.Address_ID.Address_City
+					request.session['Address_Street'] = user.Address_ID.Address_Street
+
 
 					return redirect('http://127.0.0.1:8000/user-profile')
 				else:
@@ -86,7 +100,19 @@ class Functions(View):
 		if request.method == "POST":
 			user = User.objects.get(Email = request.POST['Email'])
 			if user.Password == request.POST['Password']:
+				request.session['User_ID'] = user.User_ID
 				request.session['First_Name'] = user.First_Name
+				request.session['Last_Name'] = user.Last_Name
+				request.session['Email'] = user.Email
+				request.session['Birthdate'] = user.Birthdate
+				request.session['Mobile_Number'] = user.Mobile_Number
+				request.session['Balance'] = user.Balance
+				request.session['Credit_Score'] = user.Credit_Score
+
+				request.session['Address_Province'] = user.Address_ID.Address_Province
+				request.session['Address_City'] = user.Address_ID.Address_City
+				request.session['Address_Street'] = user.Address_ID.Address_Street
+				
 				return redirect('http://127.0.0.1:8000/user-profile')
 			else:
 				return HttpResponse("Your Email and Password do not Match.")
