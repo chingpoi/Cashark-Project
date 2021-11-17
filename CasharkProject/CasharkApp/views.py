e91830e64950a36d38663e59afa2183bf270637a
chingpoi <87354186+chingpoi@users.noreply.github.com>
2021-11-18 00:05:00 +0800

    Ver 1.3.5.2 - Effiecient Session
    More efficient session

--- CasharkProject/CasharkApp/views.py
+++ CasharkProject/CasharkApp/views.py
@@ -6,44 +6,143 @@ from django.http import HttpResponse
 # Create your views here.
 class AboutView(View):
 	def get(self,request):
+		try:
+			request.session['User_ID']
+			currentUser = User.objects.get(User_ID = request.session['User_ID'])
+			user = User.objects.get(User_ID = currentUser.User_ID)
+			context = {
+				'user': user,
+			}
+			return render(request,'about.html',context)
+		except KeyError:
+			pass
 		return render(request,'about.html')
+		
 
 class BlogSingleView(View):
 	def get(self,request):
+		try:
+			request.session['User_ID']
+			currentUser = User.objects.get(User_ID = request.session['User_ID'])
+			user = User.objects.get(User_ID = currentUser.User_ID)
+			context = {
+				'user': user,
+			}
+			return render(request,'blog-single.html',context)
+		except KeyError:
+			pass
 		return render(request,'blog-single.html')
 
 class BlogDoubleView(View):
 	def get(self,request):
+		try:
+			request.session['User_ID']
+			currentUser = User.objects.get(User_ID = request.session['User_ID'])
+			user = User.objects.get(User_ID = currentUser.User_ID)
+			context = {
+				'user': user,
+			}
+			return render(request,'blog-double.html',context)
+		except KeyError:
+			pass
 		return render(request,'blog-double.html')
 
 class BlogView(View):
 	def get(self,request):
+		try:
+			request.session['User_ID']
+			currentUser = User.objects.get(User_ID = request.session['User_ID'])
+			user = User.objects.get(User_ID = currentUser.User_ID)
+			context = {
+				'user': user,
+			}
+			return render(request,'blog.html',context)
+		except KeyError:
+			pass
 		return render(request,'blog.html')
 
 class ContactView(View):
 	def get(self,request):
+		try:
+			request.session['User_ID']
+			currentUser = User.objects.get(User_ID = request.session['User_ID'])
+			user = User.objects.get(User_ID = currentUser.User_ID)
+			context = {
+				'user': user,
+			}
+			return render(request,'contact.html',context)
+		except KeyError:
+			pass
 		return render(request,'contact.html')
 
 class IndexView(View):
 	def get(self,request):
+		try:
+			request.session['User_ID']
+			currentUser = User.objects.get(User_ID = request.session['User_ID'])
+			user = User.objects.get(User_ID = currentUser.User_ID)
+			context = {
+				'user': user,
+			}
+			return render(request,'index.html',context)
+		except KeyError:
+			pass
 		return render(request,'index.html')
 
 
 class LoginView(View):
 	def get(self,request):
+
 		return render(request,'login.html')
 
 class PortfolioView(View):
 	def get(self,request):
+		try:
+			request.session['User_ID']
+			currentUser = User.objects.get(User_ID = request.session['User_ID'])
+			user = User.objects.get(User_ID = currentUser.User_ID)
+			context = {
+				'user': user,
+			}
+			return render(request,'portfolio.html',context)
+		except KeyError:
+			pass
 		return render(request,'portfolio.html')
 
 class ServicesView(View):
 	def get(self,request):
+		try:
+			request.session['User_ID']
+			currentUser = User.objects.get(User_ID = request.session['User_ID'])
+			user = User.objects.get(User_ID = currentUser.User_ID)
+			context = {
+				'user': user,
+			}
+			return render(request,'services.html',context)
+		except KeyError:
+			pass
 		return render(request,'services.html')
 
 class ProfileView(View):
 	def get(self,request):
-		return render(request,'profile.html')
+		currentUser = User.objects.get(User_ID = request.session['User_ID'])
+
+		user = User.objects.get(User_ID = currentUser.User_ID)
+		bankInfo = BankInfo.objects.all()
+		transaction = Transaction.objects.all()
+		message = Message.objects.all()
+		gCash = GCash.objects.all()
+		address = Address.objects.all()
+
+		context = {
+			'user': user,
+			'bankInfo': bankInfo,
+			'transaction': transaction,
+			'message': message,
+			'gCash': gCash,
+			'address': address,
+		}
+		return render(request,'profile.html',context)
 
 
 #Functions
@@ -74,26 +173,13 @@ class Functions(View):
 					user = User.objects.get(Email = email)
 
 					request.session['User_ID'] = user.User_ID
-					request.session['First_Name'] = user.First_Name
-					request.session['Last_Name'] = user.Last_Name
-					request.session['Email'] = user.Email
-					request.session['Birthdate'] = user.Birthdate
-					request.session['Mobile_Number'] = user.Mobile_Number
-					request.session['Balance'] = user.Balance
-					request.session['Credit_Score'] = user.Credit_Score
-
-					request.session['Address_Province'] = user.Address_ID.Address_Province
-					request.session['Address_City'] = user.Address_ID.Address_City
-					request.session['Address_Street'] = user.Address_ID.Address_Street
-
-
 					return redirect('http://127.0.0.1:8000/user-profile')
 				else:
 					print(form.errors)
 					return HttpResponse('not valid')
 
 	def Logout(request):
-		del request.session['First_Name']
+		del request.session['User_ID']
 		return redirect('http://127.0.0.1:8000/')
 
 	def UserLogin(request):
@@ -101,18 +187,7 @@ class Functions(View):
 			user = User.objects.get(Email = request.POST['Email'])
 			if user.Password == request.POST['Password']:
 				request.session['User_ID'] = user.User_ID
-				request.session['First_Name'] = user.First_Name
-				request.session['Last_Name'] = user.Last_Name
-				request.session['Email'] = user.Email
-				request.session['Birthdate'] = user.Birthdate
-				request.session['Mobile_Number'] = user.Mobile_Number
-				request.session['Balance'] = user.Balance
-				request.session['Credit_Score'] = user.Credit_Score
 
-				request.session['Address_Province'] = user.Address_ID.Address_Province
-				request.session['Address_City'] = user.Address_ID.Address_City
-				request.session['Address_Street'] = user.Address_ID.Address_Street
-				
 				return redirect('http://127.0.0.1:8000/user-profile')
 			else:
 				return HttpResponse("Your Email and Password do not Match.")
